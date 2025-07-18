5d2aebe92972659be59d309487dc1cdbfa2c41c9
antistereov <andre.antimonov@stereov.io>
2025-07-18 17:06:07 +0200

    feat(fish): update configuration for fish with improved tools

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,23 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set GPG_TTY $(tty)
+set fish_greeting ""
+
+# Docker
 alias dc="docker compose"
 alias dl="docker logs"
 alias de="docker exec"
 alias dps="docker ps --format 'table {{.Names}}\t{{printf \"%-20s\" .Status}}'"
+
+# Kubernetes
 alias k kubectl
 alias kn "kubectl config set-context --current --namespace"
 
 # Enable zoxide
 zoxide init fish | source
 
+# Improve utilities
+alias rm "trash -v"
+alias ll "eza -l"
+alias ls "eza"
